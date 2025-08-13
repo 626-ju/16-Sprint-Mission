2a2dd49d63e79dbf2d78b46b641a94c5685061b1
김성주 <aaa8757k@gmail.com>
2025-08-13 18:06:36 +0900

    refactor: useSwitchTodo에 낙관적 업데이트 적용

--- src/hooks/useSwitchTodo.ts
+++ src/hooks/useSwitchTodo.ts
@@ -1,4 +1,5 @@
 import { patchTodo } from '@/service/patchTodo';
+import { Items } from '@/types/itemsType';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export function useSwitchTodo() {
@@ -7,6 +8,26 @@ export function useSwitchTodo() {
   const { mutate } = useMutation({
     mutationFn: ({ id, isCompleted }: { id: number; isCompleted: boolean }) =>
       patchTodo(id, isCompleted),
+
+    onMutate: async ({ id, isCompleted }) => {
+      await queryClient.cancelQueries({ queryKey: ['todoList'] });
+      const previous = queryClient.getQueryData(['todoList']);
+
+      queryClient.setQueryData<Items>(['todoList'], (prev) =>
+        prev?.map((item) => (item.id === id ? { ...item, isCompleted: !isCompleted } : item)),
+      );
+
+      return { previous };
+    },
+
+    onError: (_0, _1, context) => {
+      queryClient.setQueryData(['todoList'], context?.previous);
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['todoList'] });
+    },
+
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['todoList'] }),
   });
 
