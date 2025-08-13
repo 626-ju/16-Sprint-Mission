1ff39b1421a679465e9c15638599ddf66243fe89
김성주 <aaa8757k@gmail.com>
2025-08-13 16:45:23 +0900

    feat: theme.colors 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,27 +1,29 @@
 export const theme = {
   colors: {
-    slate100: "#F1F5F9",
-    
-    slate200: "#E2E8F0",
-    slate300: "#CBD5E1",
-    slate400: "#94A3B8",
-    slate500: "#64748B",
-    slate800: "#1E293B",
-    slate900: "#0F172A",
+    slate100: '#F1F5F9',
 
-    amber800: "#92400E",
-    lime300: "#BEF264",
-    rose500: "#F43F5E",
-    violet100: "#EDE9FE",
-    violet600: "#7C3AED",
+    slate200: '#E2E8F0',
+    slate300: '#CBD5E1',
+    slate400: '#94A3B8',
+    slate500: '#64748B',
+    slate800: '#1E293B',
+    slate900: '#0F172A',
 
-    gray50: "#F9FAFB",
+    amber800: '#92400E',
+    amber300: '#FCD34D',
+    green700: '#15803D',
+    lime300: '#BEF264',
+    rose500: '#F43F5E',
+    violet100: '#EDE9FE',
+    violet600: '#7C3AED',
+
+    gray50: '#F9FAFB',
   },
 
   fontSizes: {
-    md: "16px",
-    lg: "18px",
-    xl: "20px",
+    md: '16px',
+    lg: '18px',
+    xl: '20px',
   },
 };
 
